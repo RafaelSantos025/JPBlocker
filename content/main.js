18fb3ac43b714ec34d7e8b5163afef02411e356d
Rafael <45268280+RafaelSantos025@users.noreply.github.com>
2021-06-03 22:44:52 -0300

    Update main.js

--- content/main.js
+++ content/main.js
@@ -1,47 +1,102 @@
 
-var sleepTime = 3000;
-block_servers_urls_list = ["https://raw.githubusercontent.com/easylist/easylistportuguese/master/easylistportuguese/easylistportuguese_adservers.txt"]
-html_tags_list = ["img", "video", "iframe", "embed", "object", "source"]
-lists_ready = 0
-server_list = []
+sleepTime = 3000;
+blockServersUrlsList = ["https://raw.githubusercontent.com/easylist/easylist/master/easylist/easylist_adservers.txt", "https://raw.githubusercontent.com/easylistbrasil/easylistbrasil/master/easylistbrasil/adservers.txt"];
+htmlTagsList = ["img", "video", "iframe", "embed", "object", "source"];
+bypassServers = ["github", window.location.hostname.toString()]
+listsReady = 0;
+listsNumber = blockServersUrlsList.length;
+loops = 0;
+maxLoops = 5;
+serverList = [];
+jpOn = true;
 
 function main(){
-    get_blocked_servers(block_servers_urls_list);
+    getBlockedServers(blockServersUrlsList);
 
-    var interval = setInterval(() => {
-        if(lists_ready >= 1){
-            html_tags_list.forEach((tag) => {
-                block_ads(server_list, tag);
-            });
+    startJP();
+    setInterval(() => {
+        if (loops < maxLoops){
+            startJP();
         }
     }, sleepTime);
 }
 
-function parse_response(response){
+function startJP(){
+    loops += 1;
+    console.log("JP: " + loops.toString());
+    if(listsReady == listsNumber){
+        htmlTagsList.forEach((tag) => {
+            if (jpOn){
+                blockAds(serverList, tag);
+            }
+        });
+    }
+}
+
+function switchButton(){
+    if (jpOn){
+        jpOn = false;
+    }else{
+        jpOn = true;
+        loops = 0;
+    }
+}
+
+function checkServer(server){
+    server = server.replace(" ", "").replace("\n", "").replace("\r", "").replace("\t", "");
+
+    if(server==""){
+        return false;
+    }
+    if(!server.includes(".")){
+        return false;
+    }
+
+    return true;
+}
+
+function checkUrl(url){
+    if (url == ""){
+        return false;
+    }
+
+    for (i=0; i<bypassServers.length; i++){
+        s =  bypassServers[i];
+        if (url.includes(s)){
+            console.log("BYPASSED: " + url);
+            return false;
+        }
+    }
+
+    return true;
+}
+
+function parseResponse(response){
     var splited = response.split("\n");
-    str_server_list = ""
+    strServerList = ""
 
     splited.forEach((s) => {
         if (s.includes("||")){
             server = s.replace("||", "").split("^")[0].split("/")[0];
-            str_server_list += server + ","
+            if (checkServer(server)){
+                strServerList += server + ","
+            }
         }
-        
     });
 
-    server_list = server_list.concat(str_server_list.replace(" ", "").replace(window.location.hostname.toString(), "").split(","));
-    lists_ready += 1;
+    serverList = serverList.concat(strServerList.replace(" ", "").replace(window.location.hostname.toString(), "").split(","));
+    listsReady += 1;
 }
 
-function get_blocked_servers(urls){
+function getBlockedServers(urls){
 
     urls.forEach((url) => {
         var xhttp = new XMLHttpRequest();
 
         xhttp.onreadystatechange = function() {
             if (this.readyState == 4 && this.status == 200) {
-                raw_response = xhttp.responseText;
-                parse_response(raw_response);
+                rawResponse = xhttp.responseText;
+                parseResponse(rawResponse);
             }
         }
 
@@ -50,22 +105,36 @@ function get_blocked_servers(urls){
     });
 }
 
-function block_ads(banned_ads_servers, html_tag){
-    imgTags = document.querySelectorAll(html_tag);
+function blockAds(bannedAdsServers, htmlTag){
+    imgTags = document.querySelectorAll(htmlTag);
 
     imgTags.forEach((tag) => {
-        banned_ads_servers.forEach((server) => {
-            if (!server == ""){
-                if (html_tag == "object" && tag.data.toString().includes(server)){
-                    console.log("BLOCKED: " + tag.data.toString());
-                    tag.data = "";
-                }
-                else if (tag.src.toString().includes(server)){ 
-                    console.log("BLOCKED: " + tag.src.toString());
-                    tag.src = "";
+        if (htmlTag == "object" && tag.data != undefined){
+            if (checkUrl(tag.data.toString())){
+                for (i=0; i<bannedAdsServers.length; i++){
+                    server = bannedAdsServers[i];
+                    if (tag.data.toString().includes(server)){
+                        console.log("BLOCKED: " + tag.data.toString());
+                        tag.data = "";
+                        break;
+                    }
                 }
             }
-        });
+        }
+        else if (tag.src != undefined){
+            url = tag.src.toString();
+            
+            if (checkUrl(tag.src.toString())){
+                for (i=0; i<bannedAdsServers.length; i++){
+                    server = bannedAdsServers[i];
+                    if (tag.src.toString().includes(server)){
+                        console.log("BLOCKED: " + tag.src.toString());
+                        tag.src = "";
+                        break;
+                    }
+                }
+            }
+        }
     });
 }
 
