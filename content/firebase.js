3ea8dc8e9a803e664c59f07db68fdcefb38ff941
Rafael <45268280+RafaelSantos025@users.noreply.github.com>
2021-06-03 22:45:35 -0300

    Create firebase.js

---
+++ content/firebase.js
@@ -0,0 +1,8 @@
+firebaseConfig = {
+    apiKey: "AIzaSyBLWYpXhub3TmjD2HhPGn34D_uQ3a_mGG0",
+    authDomain: "jpblocker-45867.firebaseapp.com",
+    projectId: "jpblocker-45867"
+};
+
+firebase.initializeApp(firebaseConfig);
+var db = firebase.firestore();
